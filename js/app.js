f34faac2717ae9089b5cc7b944a8013c7b059e0c
zaithottakath <zaithottakath@gmail.com>
2014-11-09 21:46:39 -0800

    Trying to get ember working

---
+++ js/app.js
@@ -0,0 +1,15 @@
+App = Ember.Application.create();
+
+App.Router.map(function() {
+  // put your routes here
+  // this.resouce('posts');
+});
+
+
+// App.PostsRoute = Ember.Route.extend({
+//   model: function(){
+//     return $.getJSON("/feed.json").done(function(data){
+//      return data;
+//    });
+//   }
+// });
